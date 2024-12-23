b0f451ce4b87f2095815231d56eb0787a92de9b4
Aaron Wilson <acwilsoncs@gmail.com>
2024-12-23 07:48:41 -0700

    clean: add tf to gitignore

---
+++ terraform/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "acwilson-core-terraform"
+    key            = "acwilson-inventory-management-demo.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+}
