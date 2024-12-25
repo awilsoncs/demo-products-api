976d0df3439d35474ed41fcce835811473f9ff18
Aaron Wilson <acwilsoncs@gmail.com>
2024-12-25 12:18:05 -0700

    revising to use serverless framework

--- terraform/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "api_gateway_url" {
-  value = aws_api_gateway_deployment.inventory_api_deployment.invoke_url
-}
--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "region" {
-  description = "The AWS region to deploy to"
-  default     = "us-east-1"
-}
-
-variable "app_name" {
-  description = "The name of the application"
-  default     = "inventory-management-demo"
-}
