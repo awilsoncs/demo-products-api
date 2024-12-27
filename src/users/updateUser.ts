cbf91d5b90ed7a0a923a59ccc44a76ccce0cbb2c
Aaron Wilson <acwilsoncs@gmail.com>
2024-12-26 17:27:31 -0700

    fix: add update user back in

---
+++ src/users/updateUser.ts
@@ -0,0 +1,62 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
+import { DynamoDB } from 'aws-sdk';
+
+export const handler = async (event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> => {
+    const tableName = process.env.INVENTORY_TABLE_NAME;
+    const dynamoDb = new DynamoDB.DocumentClient();
+
+    if (!event.body) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ error: 'Invalid request, no body provided' }),
+        };
+    }
+
+    const { userId, name, email } = JSON.parse(event.body);
+
+    if (!userId) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ error: 'User ID is required' }),
+        };
+    }
+
+    if (!tableName) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Table name is not defined in environment variables' }),
+        };
+    }
+
+    const params = {
+        TableName: tableName,
+        Key: { userId },
+        UpdateExpression: 'set #name = :name, #email = :email, #updated_at = :updated_at',
+        ExpressionAttributeNames: {
+            '#name': 'name',
+            '#email': 'email',
+            '#updated_at': 'meta.updated_at',
+        },
+        ExpressionAttributeValues: {
+            ':name': name,
+            ':email': email,
+            ':updated_at': new Date().toISOString(),
+        },
+        ReturnValues: 'ALL_NEW',
+    };
+
+    try {
+        const result = await dynamoDb.update(params).promise();
+        console.log(result);
+        return {
+            statusCode: 200,
+            body: JSON.stringify(result.Attributes),
+        };
+    } catch (error) {
+        console.error(error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Could not update user' }),
+        };
+    }
+};
