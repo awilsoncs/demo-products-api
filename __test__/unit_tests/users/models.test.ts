f0f12b30e5e2a35efdb3167ee6a79f80fb0d1576
Aaron Wilson <acwilsoncs@gmail.com>
2024-12-27 14:51:06 -0700

    refactor(models): move meta to shared package

--- __test__/unit_tests/users/models.test.ts
+++ __test__/unit_tests/users/models.test.ts
@@ -1,4 +1,5 @@
-import { LoginUser, User, Meta } from '../../../src/users/models';
+import { LoginUser, User } from '../../../src/users/models';
+import { Meta } from '../../../src/shared/models';
 
 describe('LoginUser', () => {
   it('should create a LoginUser instance from JSON', () => {
@@ -46,34 +47,5 @@ describe('User', () => {
     expect(new Date(json.meta.createdAt).toISOString()).toBe(meta.createdAt.toISOString());
     expect(json.meta.updatedBy).toBe('admin');
     expect(new Date(json.meta.updatedAt).toISOString()).toBe(meta.updatedAt.toISOString());
-  });
-});
-
-describe('Meta', () => {
-  it('should create a Meta instance from JSON', () => {
-    const json = {
-      createdBy: 'admin',
-      createdAt: new Date().getMilliseconds(),
-      updatedBy: 'admin',
-      updatedAt: new Date().getMilliseconds(),
-    };
-    const meta = Meta.fromJson(json);
-
-    expect(meta.createdBy).toBe('admin');
-    expect(meta.createdAt.getMilliseconds()).toBe(json.createdAt);
-    expect(meta.updatedBy).toBe('admin');
-    expect(meta.updatedAt.getMilliseconds()).toBe(json.updatedAt);
-  });
-
-  it('should convert a Meta instance to JSON', () => {
-    const createdAt = new Date();
-    const updatedAt = new Date();
-    const meta = new Meta('admin', createdAt, 'admin', updatedAt);
-    const json = Meta.toJson(meta);
-
-    expect(json.createdBy).toBe('admin');
-    expect(new Date(json.createdAt).toISOString()).toBe(createdAt.toISOString());
-    expect(json.updatedBy).toBe('admin');
-    expect(new Date(json.updatedAt).toISOString()).toBe(updatedAt.toISOString());
   });
 });
--- src/users/models.ts
+++ src/users/models.ts
@@ -1,3 +1,5 @@
+import { Meta } from "../shared/models";
+
 class LoginUser {
   readonly email: string;
   readonly password: string;
@@ -39,35 +41,4 @@ class User {
   }
 }
 
-class Meta {
-  readonly createdBy: string;
-  readonly createdAt: Date;
-  readonly updatedBy: string;
-  readonly updatedAt: Date;
-
-  constructor(createdBy: string, createdAt: Date, updatedBy: string, updatedAt: Date) {
-      this.createdBy = createdBy;
-      this.createdAt = createdAt;
-      this.updatedBy = updatedBy;
-      this.updatedAt = updatedAt;
-  }
-
-  static fromJson(json: any): Meta {
-      return new Meta(
-        json.createdBy,
-        new Date(json.createdAt),
-        json.updatedBy,
-        new Date(json.updatedAt));
-  }
-
-  static toJson(meta: Meta): any {
-      return {
-          createdBy: meta.createdBy,
-          createdAt: meta.createdAt.toISOString(),
-          updatedBy: meta.updatedBy,
-          updatedAt: meta.updatedAt.toISOString(),
-      };
-  }
-}
-
-export { LoginUser, User, Meta };
+export { LoginUser, User };
